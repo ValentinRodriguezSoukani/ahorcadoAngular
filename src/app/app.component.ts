ccaa693de14fc5019e9481536332d831b2c131a4
Vanesa Romero - GH <vaparova_92@hotmail.com>
2023-04-10 16:40:25 -0300

    0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ahorcadoAngular';
+  arrCoincidencias = []; // esto es para corregir!
+  palabraAdivinar = 'hola';
+
+  constructor(){
+    
+  }
 }
